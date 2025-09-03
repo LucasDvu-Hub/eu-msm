2246808dd78f9ae4e0c7dca9d21c714e5f52ead8
LucasDvu-Hub <lucas.dvulathca@escola.pr.gov.br>
2025-09-03 20:10:51 -0300

    Add files via upload

---
+++ Scrip.js
@@ -0,0 +1,37 @@
+//tipos de dados
+//tipo STRING: TEXTO. IDENTIFICADO PELO USO DAS ASPAS 
+//DUPLAS
+let nome = "Lucas";
+
+//tipo NUMBER: se trata de número.
+
+let num = 10;
+let num2 = "10";
+
+//tipo boleano: aceita dois valores como referencia e retorna um dado com base na condição.
+// um dado com base na condição.
+
+//let ave = "pinguim";
+//if (ave = true){
+   // alert("é uma ave.")
+//} else {
+  //  alert("não é uma ave");
+//}
+
+alert("ola seja bem vindo ao meu site")
+alert (" prescisamos confirmar alguns dados antes de começar")
+ let seuNome = prompt("digite seu nome por favor");
+
+ alert("seja bem vindo " + seuNome + " ao meu site"); //usamos concatenação
+ alert("Agora prescisamos saber a sua idade!");
+ let idade = prompt("Por favor digite a sua idade");
+ alert(" Ja sei que você tem " + idade + "e agora presciso do seu e-mail!");
+ let email = prompt("Digite seu e-mail");
+ alert("seu email é" + email);
+ 
+ console.log(nome);
+ console.log(num);
+ console.log(num2);
+ console.log(seuNome);
+ console.log(idade);
+ console.log(email);
